82e0f2462beb4cc4e5142e728e86cdf91c938e71
acaciomartins <acacio.am@gmail.com>
2020-01-16 22:31:39 -0300

    BackEnd ajuste cors e Implementaçao da Web com ReactJs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 const routes = require('./routes');
 
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
 // Para todas as rotas - Aceita Json no body
 app.use(express.json());
 app.use(routes);
